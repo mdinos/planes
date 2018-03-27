8a8f5a8fac55d19d6fde4fa83a59ce41539cb4ef
mdinos <up839812@myport.ac.uk>
2018-03-27 21:46:01 +0100

    Finally got around git's issues

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const express = require('express')
 const session = require('express-session')
 const mysql = require('mysql')
 const path = require('path')
+const passwordHash = require('password-hash')
 const app = express()
 
 //database initialisation
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: 'password'
 })
+
+// get authorization
+//let auth.res
 
 // database connection
 connection.connect(function(err) {
@@ -32,8 +36,8 @@ connection.query('CREATE DATABASE IF NOT EXISTS planesdb', function (err) {
             + 'id INT NOT NULL AUTO_INCREMENT,'
             + 'PRIMARY KEY(id),'
             + 'username VARCHAR(30) NOT NULL,'
-            + 'password VARCHAR(30) NOT NULL,'
-            + 'email VARCHAR(50) NOT NULL'
+            + 'password VARCHAR(150) NOT NULL,'
+            + 'email VARCHAR(150) NOT NULL'
             +  ') ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;', function (err) {
                 if (err) {
                     console.log("Error:")
@@ -56,10 +60,49 @@ app.use(session({
 }))
 
 // GET REQUESTS
-
 // POST REQUESTS
+app.post('/api/login', login)
+app.post('/api/createuser', createUser)
 
 // Listen at 127.0.0.1:8080
 app.listen(8080, function() {
     console.log("Server started, port 8080.")
 })
+
+// SERVER FUNCTIONS
+
+function login(req, res) {
+    let username = req.query.username.toLowerCase()
+    let password = req.query.password
+    
+    connection.query('SELECT * FROM users WHERE username = "' + username + '"', function (err, results) {
+        if (err) {
+            console.error(err)
+        }
+        else {
+            console.log(results[0].id)
+            console.log(passwordHash.verify(password, results[0].password))
+        }
+    })
+}
+
+function createUser(req, res) {
+    let username = req.query.username.toLowerCase()
+    let password = req.query.password
+    let email = req.query.email.toLowerCase()
+    
+    // hashing passwords prior to passing to the database for storage
+    let hashedPassword = passwordHash.generate(password)
+    
+    connection.query('INSERT INTO users '
+    + '(username, password, email)'
+    + ' VALUES ("' + username + '", "' + hashedPassword + '", "' + email + '")', function (err) {
+        if (err) {
+            console.log("Error inserting user data into table 'users'.")
+            console.error(err)
+        }
+        else {
+            console.log("User created: " + username)
+        }
+    })
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-function createAccount() {
+async function createAccount() {
     // regex for checking validity of inputs
     let usernamePattern = /[A-Za-z0-9]{4,15}/
     let pwPattern = /^(?=.*\d).{6,30}$/
@@ -11,30 +11,51 @@ function createAccount() {
     let password1 = document.getElementById('password').value
     let password2 = document.getElementById('password2').value
     
-    // test user information for validity
+    // test user information for validity, and send errors from the top to the bottom of the form
     if (!usernamePattern.test(username)) {
         sendErrorToUser("Invalid username! Please ensure your name meets the criteria!")
+        return false
     }
     else {
-        console.log("username valid")
         if (email1 != email2) {
             sendErrorToUser("Emails do not match! Please ensure you have typed you email correctly.")
+            return false
         }
         else {
             if (!emailPattern.test(email1)) {
                 sendErrorToUser("Email invalid, please ensure your email is correct.")
+                return false
             }
             else {
-                console.log("email ok")
                 if (password1 != password2) {
                     sendErrorToUser("Passwords do not match! Please retype your password.")
+                    return false
                 }
                 else {
-                    if (pwPattern.test(password1)) {
-                        console.log(password1);
+                    if (!pwPattern.test(password1)) {
+                        sendErrorToUser("Password invalid, please ensure you have fulfilled the password creation criteria.")
+                        return false
                     }
                     else {
-                        sendErrorToUser("Password invalid, please ensure you have fulfilled the password creation criteria.")
+                        let url = '/api/createuser'
+                        
+                        url += '?username=' + username
+                        url += '&password=' + password1
+                        url += '&email=' + email1
+                        
+                        const fetchOptions = { method: 'POST' }
+                        const response = await fetch(url, fetchOptions)
+                        console.log(response + " RESPONSE")
+                        if (!response.ok) {
+                            console.log("There's something wrong with /api/createuser!")
+                            sendErrorToUser("Oops! Something went wrong, trust me, I'm working on it!");
+                            return false
+                        }
+                        else {
+                            console.log("Should be fine!");
+                            window.location.replace('login.html')
+                            return true
+                        }
                     }
                 }
             }
@@ -46,4 +67,19 @@ function sendErrorToUser(error) {
     console.log(error)
     let errorzone = document.getElementById('errorzone')
     errorzone.innerHTML = error
+}
+
+async function signIn() {
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
+    
+    let url = '/api/login'
+    
+    url += '?username=' + username
+    url += '&password=' + password
+    
+    const fetchOptions = { method: 'POST' }
+    const response = await fetch(url, fetchOptions)
+    
+    console.log(response);
 }
